274273be59b25128c78dbe2ad473bafc27f95a17
Thomas Binetruy <tbinetruy@gmail.com>
2017-08-16 15:05:14 -0400

    updated gantt tasks

--- src/App.js
+++ src/App.js
@@ -6,74 +6,107 @@ import Gantt from './gantt/Gantt';
 
 class App extends Component {
   render() {
-    const tasks = [
+    const initDate = () => new Date('10/09/17');
+    const week = weeks => new Date(new Date(initDate()).setDate(initDate().getDate() + 7 * weeks));
+
+    let tasks = [
       {
         title: "Hardware install",
-        startDate: new Date('01/05/17'),
-        endDate: new Date('01/18/17'),
+        startDate: week(0),
+        endDate: week(2),
         color: '#65B766',
         subtasks: [
-          {
-            title: "Setch design",
-            startDate: new Date('01/05/17'),
-            endDate: new Date('01/09/17'),
-          },
-          {
-            title: "Intro tech restrictions",
-            startDate: new Date('01/09/17'),
-            endDate: new Date('01/18/17'),
-          }
         ]
       },
       {
         title: "Define expectation",
-        startDate: new Date('01/18/17'),
-        endDate: new Date('01/31/17'),
+        startDate: week(0),
+        endDate: week(3),
         color: '#59AF92',
         subtasks: [
-          {
-            title: "Sketch design",
-            startDate: new Date('01/18/17'),
-            endDate: new Date('01/25/17'),
-          },
-          {
-            title: "Intro tech restrictions",
-            startDate: new Date('01/25/17'),
-            endDate: new Date('01/31/17'),
-          }
         ]
       },
       {
         title: "Exp BGRS tour",
-        startDate: new Date('01/18/17'),
-        endDate: new Date('02/08/17'),
+        startDate: week(3),
+        endDate: week(5),
         color: '#59A4AF',
         subtasks: []
       },
       {
         title: "Hard Mop creative",
-        startDate: new Date('01/18/17'),
-        endDate: new Date('03/19/17'),
+        startDate: week(5),
+        endDate: week(13),
         color: '#5888B3',
         subtasks: [
           {
             title: "Sketch design",
-            startDate: new Date('01/18/17'),
-            endDate: new Date('01/25/17'),
+            startDate: week(5),
+            endDate: week(6),
           },
           {
             title: "Intro tech restrictions",
-            startDate: new Date('01/25/17'),
-            endDate: new Date('01/31/17'),
+            startDate: week(5),
+            endDate: week(6),
           },
           {
             title: "Gameplay",
-            startDate: new Date('01/20/17'),
-            endDate: new Date('01/28/17'),
-          }
+            startDate: week(5),
+            endDate: week(10),
+          }, 
+          {
+            title: "Environments",
+            startDate: week(6),
+            endDate: week(8),
+          }, 
+          {
+            title: "Storyline",
+            startDate: week(5),
+            endDate: week(9),
+          }, 
+          {
+            title: "Concept art",
+            startDate: week(6),
+            endDate: week(13),
+          }, 
         ]
       },
+      {
+        title: "Development",
+        startDate: week(6),
+        endDate: week(13),
+        color: '#5888B3',
+        subtasks: [
+          {
+            title: "Integration",
+            startDate: week(6),
+            endDate: week(11),
+          },
+          {
+            title: "Asset integration",
+            startDate: week(8),
+            endDate: week(12),
+          },
+          {
+            title: "Testing",
+            startDate: week(9),
+            endDate: week(13),
+          }, 
+        ],
+      },
     ];
+
+    const orderTasks = tasks => {
+      tasks.sort((a, b) => a.startDate - b.startDate);
+
+      for(let i = 0; i < tasks.length; i++) {
+        tasks[i].subtasks.sort((a, b) => a.startDate - b.startDate);
+      }
+
+      return tasks;
+    }
+
+    tasks = orderTasks(tasks);
     return (
       <div className="App">
         <Gantt tasks={ tasks } />

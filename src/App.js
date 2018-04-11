093b5e79ff7e1bc41164a4f4678b9c539f6b312d
Thomas Binetruy <tbinetruy@gmail.com>
2018-04-10 21:43:11 -0400

    should be part of tasks refactor in sep. file commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      initDate: '09/17/2017',
+      initDate: startDate,
       getInitDate : () => new Date(this.state.initDate),
       base: {
         x: 10,
@@ -45,99 +45,7 @@ class App extends Component {
     this.getGanttSizeControls = this.getGanttSizeControls.bind(this);
 
     const week = this.weeksToDate;
-    this.state.tasks = this.orderTasks([
-      {
-        title: "Hardware install",
-        startDate: week(0),
-        endDate: week(3),
-        color: '#65B766',
-        subtasks: [
-        ]
-      },
-      {
-        title: "Needs analysis",
-        startDate: week(0),
-        endDate: week(4),
-        color: '#59AF92',
-        subtasks: [
-        ]
-      },
-      {
-        title: "BGRS tour",
-        startDate: week(1),
-        endDate: week(4),
-        color: '#59A4AF',
-        subtasks: []
-      },
-      {
-        title: "Design spec",
-        startDate: week(3),
-        endDate: week(15),
-        color: '#5888B3',
-        subtasks: [
-          {
-            title: "Script",
-            startDate: week(3),
-            endDate: week(7),
-          }, 
-          {
-            title: "Graphic charters",
-            startDate: week(3),
-            endDate: week(11),
-          },
-          {
-            title: "Feasibility analysis",
-            startDate: week(14),
-            endDate: week(15),
-          },
-          {
-            title: "Technical specification",
-            startDate: week(5),
-            endDate: week(14),
-          }, 
-        ]
-      },
-      {
-        title: "Training",
-        startDate: week(2),
-        endDate: week(14),
-        color: '#6b71b4',
-        subtasks: [
-          {
-            title: 'Getting started',
-            startDate: week(2),
-            endDate: week(4),
-          },
-          {
-            title: 'Hardware',
-            startDate: week(4),
-            endDate: week(7),
-          },
-          {
-            title: 'Software',
-            startDate: week(7),
-            endDate: week(10),
-          },
-          {
-            title: 'Theory',
-            startDate: week(10),
-            endDate: week(13),
-          },
-          {
-            title: 'Lab',
-            startDate: week(13),
-            endDate: week(14),
-          },
-        ]
-      },
-      {
-        title: 'Final restitution',
-        startDate: week(15),
-        endDate: week(16),
-        subtasks: [],
-        color: '#926cad',
-      }
-    ]);
+    this.state.tasks = this.orderTasks(getTasks(week));
   }
 
   orderTasks(tasks_original) {
